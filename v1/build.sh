471bc900522afca27fdafb2676c14070d6cece70
Anger Jérémy <angerj.dev@gmail.com>
2022-07-31 09:43:59 +0200

    add gpu images

--- v1/build.sh
+++ v1/build.sh
@@ -2,16 +2,26 @@
 
 set -eu
 
-docker build . -f py3.7.dockerfile -t ipol:v1-py3.7
-docker build . -f py3.8.dockerfile -t ipol:v1-py3.8
-docker build . -f py3.9.dockerfile -t ipol:v1-py3.9
+registry=registry.ipol.im
 
-docker build . -f py3.7-tensorflow.dockerfile -t ipol:v1-py3.7-tensorflow
-docker build . -f py3.8-tensorflow.dockerfile -t ipol:v1-py3.8-tensorflow
-docker build . -f py3.9-tensorflow.dockerfile -t ipol:v1-py3.9-tensorflow
+docker build . -f py3.7.dockerfile -t $registry/ipol:v1-py3.7
+docker build . -f py3.8.dockerfile -t $registry/ipol:v1-py3.8
+docker build . -f py3.9.dockerfile -t $registry/ipol:v1-py3.9
 
-docker build . -f py3.7-pytorch.dockerfile -t ipol:v1-py3.7-pytorch
-docker build . -f py3.8-pytorch.dockerfile -t ipol:v1-py3.8-pytorch
-docker build . -f py3.9-pytorch.dockerfile -t ipol:v1-py3.9-pytorch
+docker build . -f py3.7-tensorflow.dockerfile -t $registry/ipol:v1-py3.7-tensorflow
+docker build . -f py3.8-tensorflow.dockerfile -t $registry/ipol:v1-py3.8-tensorflow
+docker build . -f py3.9-tensorflow.dockerfile -t $registry/ipol:v1-py3.9-tensorflow
 
-docker build . -f octave.dockerfile -t ipol:v1-octave
+docker build . -f py3.7-pytorch.dockerfile -t $registry/ipol:v1-py3.7-pytorch
+docker build . -f py3.8-pytorch.dockerfile -t $registry/ipol:v1-py3.8-pytorch
+docker build . -f py3.9-pytorch.dockerfile -t $registry/ipol:v1-py3.9-pytorch
+
+docker build . -f py3.8-gpu.dockerfile -t $registry/ipol:v1-py3.8-gpu
+docker build . -f py3.8-pytorch-gpu.dockerfile -t $registry/ipol:v1-py3.8-pytorch-gpu
+docker build . -f py3.8-tensorflow-gpu.dockerfile -t $registry/ipol:v1-py3.8-tensorflow-gpu
+
+docker build . -f octave.dockerfile -t $registry/ipol:v1-octave
+
+for i in $registry/ipol:v1-{py3.{7,8,9}{,-tensorflow,-pytorch},octave} $registry/ipol:v1-py3.8-gpu $registry/ipol:v1-py3.8-pytorch-gpu $registry/ipol:v1-py3.8-tensorflow-gpu; do
+    docker push $i
+done
---
+++ v1/py3.8-tensorflow-gpu.dockerfile
@@ -0,0 +1,18 @@
+FROM nvidia/cuda:11.2.2-cudnn8-devel-ubuntu20.04
+
+ENV LANG C.UTF-8
+
+COPY packages.txt packages.txt
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y python3 python3-pip $(cat packages.txt) && rm -rf /var/lib/apt/lists/*
+RUN update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+
+RUN pip install --no-cache-dir pip==22.0.4
+
+COPY requirements-py3.8.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+RUN wget https://github.com/quarto-dev/quarto-cli/releases/download/v0.9.106/quarto-0.9.106-linux-amd64.deb \
+&& dpkg -i quarto-0.9.106-linux-amd64.deb \
+&& rm quarto-0.9.106-linux-amd64.deb
+
+RUN pip install --no-cache-dir tensorflow==2.8.0
