76d0c9014cc5a85efcfce5422502f3cc650f7da6
Anger Jérémy <angerj.dev@gmail.com>
2024-02-18 11:11:54 +0100

    add v2 (python 3.9, 3.10, 3.11)

---
+++ v2/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eu
+
+registry=registry.ipol.im
+
+docker build . -f py3.9.dockerfile -t $registry/ipol:v2-py3.9
+docker build . -f py3.9-pytorch.dockerfile -t $registry/ipol:v2-py3.9-pytorch
+docker build . -f py3.9-tensorflow.dockerfile -t $registry/ipol:v2-py3.9-tensorflow
+
+docker build . -f py3.10.dockerfile -t $registry/ipol:v2-py3.10
+docker build . -f py3.10-pytorch.dockerfile -t $registry/ipol:v2-py3.10-pytorch
+docker build . -f py3.10-tensorflow.dockerfile -t $registry/ipol:v2-py3.10-tensorflow
+
+docker build . -f py3.11.dockerfile -t $registry/ipol:v2-py3.11
+docker build . -f py3.11-pytorch.dockerfile -t $registry/ipol:v2-py3.11-pytorch
+docker build . -f py3.11-tensorflow.dockerfile -t $registry/ipol:v2-py3.11-tensorflow
+
+docker build . -f py3.11-gpu.dockerfile -t $registry/ipol:v2-py3.11-gpu
+docker build . -f py3.11-pytorch-gpu.dockerfile -t $registry/ipol:v2-py3.11-pytorch-gpu
+docker build . -f py3.11-tensorflow-gpu.dockerfile -t $registry/ipol:v2-py3.11-tensorflow-gpu
+
+docker build . -f octave.dockerfile -t $registry/ipol:v2-octave
+
+for i in $registry/ipol:v2-{py3.{9,10,11}{,-tensorflow,-pytorch},octave} $registry/ipol:v2-py3.11-gpu $registry/ipol:v2-py3.11-pytorch-gpu $registry/ipol:v2-py3.11-tensorflow-gpu; do
+    docker push "$i"
+done
---
+++ v2/py3.10-tensorflow.dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10-bookworm
+
+RUN pip install --no-cache-dir pip==24.0
+
+COPY packages.txt packages.txt
+RUN apt-get update && apt-get install -y $(cat packages.txt) && rm -rf /var/lib/apt/lists/*
+
+COPY requirements-py3.10.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+RUN wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.4.550/quarto-1.4.550-linux-amd64.deb \
+&& dpkg -i quarto-1.4.550-linux-amd64.deb \
+&& rm quarto-1.4.550-linux-amd64.deb
+
+RUN pip install --no-cache-dir tensorflow==2.15.0.post1
